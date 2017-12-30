efbcc37cf369107ad8d9116f4f84071afa8696f1
Tanyoung Kim <tanykim@fb.com>
2017-12-29 16:00:49 -0800

    Add Flexboxgrid lib and scss color lists

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,39 +1,65 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import Map from './Map.react';
 import RidgePlots from './RidgePlots.react';
-import Park from './Park.react';
-// import './App.css';
 
 const data = require('./data/data.json');
 
 class App extends Component {
+  _wrapper = {
+    map: null,
+    ridgePlots: null
+  };
+  state = {park: ''};
 
-  constructor(props) {
-    super(props);
-    this.state = {park: '', visitor: null};
+  _getWidth = (type) => {
+    return this._wrapper[type]=== null ? 800 : this._wrapper[type].offsetWidth;
   }
 
   _onSelectPark(event) {
     const parkId = event.currentTarget.value;
-    const weather = data.weather[parkId];
     this.setState({
       park: parkId,
-      temp: weather.map(d => d.data.temperature).map(d => d.mean),
-      prec: weather.map(d => d.data.precipitation).map(d => d.precipitation[1]),
-      snow: weather.map(d => d.data.precipitation).map(d => d.snow_depth[1]),
-      // temp: weather.map(d => d.data.temperature).map(d => [d.max[1], d.mean[1], d.min[1]]),
-      // prec: weather.map(d => d.data.precipitation).map(d => [d.precipitation[1], d.snow_depth[1]]),
-      visitor: data.parks.filter(park => park.id === parkId)[0].by_month,
     });
   }
 
   render() {
     return (
-      <div className="App">
-        <select onChange={e => this._onSelectPark(e)} value={this.state.park}>
-          {data.parks.map((park, i) => <option key={i} value={park.id}>{park.name} ({park.state}, {park.id})</option>)}
-        </select>
-        <Park {...this.state} />
-        <RidgePlots data={data} selectedPark={this.state.park} />
+      <div>
+        <div className="row">
+          <div className="col-xs-12 col-md-7 col-lg-8 first-md app-map">
+            <div ref={div => {this._wrapper.map = div;}} />
+            <Map
+              data={data}
+              selectedPark={this.state.park}
+              getWidth={this._getWidth} />
+          </div>
+          <div className="col-xs-12 col-md-5 col-lg-4 first-xs app-title">
+            <div>
+              <h1> National Parks & Reactional Visitors </h1>
+            </div>
+            <select
+              onChange={e => this._onSelectPark(e)}
+              value={this.state.park}>
+              {data.parks.map((park, i) => <option key={i}
+                value={park.id}>{park.name} ({park.state}, {park.id})
+                </option>)}
+            </select>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-12 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
+            <div ref={div => {this._wrapper.ridgePlots = div;}} />
+            <RidgePlots
+              data={data}
+              selectedPark={this.state.park}
+              getWidth={this._getWidth} />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-12">
+            FOOTER
+          </div>
+        </div>
       </div>
     );
   }
--- react-app/src/RidgePlots.react.js
+++ react-app/src/RidgePlots.react.js
@@ -7,145 +7,13 @@ import * as topojson from 'topojson';
 import _ from 'lodash';
 import React, { Component } from 'react';
 
-const world = require('./data/worldmap-110m.json');
-const usa = require('./data/usa-110m.json');
-
 class RidgePlots extends Component {
 
-  // mapping
-  projection = d3.geoMercator();
-  path = d3.geoPath().projection(this.projection)
-  zoom = d3.zoom().scaleExtent([0.3, 4])
-    .on('zoom', () => {
-      // scale up the boundary path
-      d3
-        .select('.svg-g')
-        .style('stroke-width', 1.2 / d3.event.transform.k + 'px')
-        .attr('transform', d3.event.transform);
-    });
-
-  _drawOverview(data, dim) {
-    // max total points
-    const maxTotal = data.max_total_visitor;
-    const valAxis = d3.scaleLinear().range([0, dim.h / 3]).domain([0, maxTotal]);
-
-    const g = d3.select('.svg-g');
-    // add line graph by each byLat
-    for (const i in data.by_latittude) {
-      const lat = data.by_latittude[i];
-
-      // draw the group of base lines at each latitude
-      // clip the path with the boundary of United States
-      g.selectAll(`.js-lat-lines-${i}`)
-        .data(lat.base_lines)
-        .enter()
-        .append('line')
-        .attr('x1', (d) => this.projection([d[0], lat.lat_rounded])[0])
-        .attr('y1', (d) => this.projection([d[0], lat.lat_rounded])[1])
-        .attr('x2', (d) => this.projection([d[1], lat.lat_rounded])[0])
-        .attr('y2', (d) => this.projection([d[1], lat.lat_rounded])[1])
-        .attr('stroke', lat.parks_data ? 'green' : '#d0e2aa')
-        .attr('stroke-width', 1.2)
-        .attr('class', `js-lat-lines-${i}`)
-        .attr('clip-path', 'url(#map-clip-path)');
-
-      // draw each park, they are not clipped
-      const line = d3.line()
-        .x(d => this.projection([d[0], 0])[0])
-        .y(d => this.projection([0, lat.lat_rounded])[1] - valAxis(d[1]))
-        .curve(d3.curveMonotoneX);
-
-      if (lat.parks_data) {
-        g.selectAll(`.js-lat-parks-${i}`)
-          .data(lat.parks_data)
-          .enter()
-          .append('path')
-          .datum(d => d)
-          .attr('d', line)
-          .attr('stroke', 'green')
-          .attr('stroke-width', 1.2)
-          .attr('stroke-linecap', 'round')
-          .attr('fill', 'white')
-          .attr('class', `js-lat-parks-${i}`);
-      }
-    }
-
-    // max radius is 30, proportioanl to the acreage
-    const r = d3.scaleLinear().range([0, 900]).domain([0, data.max_size])
-    // add mark on each park
-    g.selectAll('circle')
-      .data(data.parks)
-      .enter()
-      .append('circle')
-      .attr('cx', d => this.projection([d.lon, d.lat])[0])
-      .attr('cy', d => this.projection([d.lon, d.lat])[1])
-      .attr('r', d => Math.sqrt(r(d.size)))
-      .style('fill', 'red')
-      .style('opacity', 0.2)
-      .on('mouseover', d => {
-        console.log(d.name, d.state, d.total);
-      });
-  }
-
-  _drawWorldMap(dim) {
-    // set the svg
-    const svg = d3
-      .select('#worldmap')
-      .attr('width', dim.w)
-      .attr('height', dim.h);
-
-    // TODO: figure out dynamically with the given dim width and height
-    this.projection.scale(dim.w * 0.6).translate([dim.w * 1.4, dim.h * 1.8]);
-
-    // prevents click to zoom
-    const stopped = () => {
-      if (d3.event.defaultPrevented) d3.event.stopPropagation();
-    };
-
-    // scroll to zoom, area of non-country on the map clicked, zoom out back.
-    svg.call(this.zoom);
-    svg.on('click', stopped, true);
-
-    // geo data
-    const CODE_USA = 840;
-    const neighbors = [124, 484]; // Candada and Mexico
-    const worldPath = topojson.feature(world, world.objects.countries).features
-      .filter(d =>  d.id === CODE_USA || neighbors.indexOf(d.id) > -1);
-    const usaPath = topojson.feature(usa, usa.objects.states).features;
-
-    // draw USA states & country boundary
-    const g = svg
-      .append('g')
-      .attr('class', 'svg-g');
-
-    g.selectAll('.js-usa-path')
-      .data(usaPath)
-      .enter()
-      .append('path')
-      .attr('d', this.path)
-      .attr('class', 'usa-path js-usa-path')
-      .style('fill', 'white')
-      .style('stroke', '#efefef');
-    g.selectAll('.js-country-path')
-      .data(worldPath)
-      .enter()
-      .append('path')
-      .attr('d', this.path)
-      .attr('class', (country) => `country-path js-country-path js-country-path-${country.id}`)
-      .style('fill', (country) => country.id === CODE_USA ? 'none' : '#efefef')
-      .style('stroke', '#cccccc');
-
-    // make a clipping mask path for the ridge plots from the united states map
-    d3.select('#map-clip-path')
-      .append('path')
-      .attr('d', d3.select(`.js-country-path-${CODE_USA}`).attr('d'));
-  }
-
-  _drawParks(data) {
+  _drawParks(w, data) {
     const plotHeight = 60; // base height of each plot
     const plotDist = 14; // distance between two plots
     const yRatio = 4; // magnifying ratio of plotting value to plot height
-    const dim = {w: 600, h: plotDist * data.parks.length};
+    const dim = {h: plotDist * data.parks.length, w};
     // have margin on the top above the line
     const margin = {top: plotHeight * yRatio + 20, right: 20, bottom: 40, left: 60};
     const g = d3.select('#ridge-plots')
@@ -188,21 +56,12 @@ class RidgePlots extends Component {
   // }
 
   componentDidMount() {
-    const dim = {w: 1600, h: 600};
-    const data = this.props.data;
-    this._drawWorldMap(dim);
-    this._drawOverview(data, dim);
-    this._drawParks(data);
+    this._drawParks(this.props.getWidth('ridgePlots'), this.props.data);
   }
 
   render() {
     return (
-      <div>
-        <svg className="map-wrapper" id="worldmap">
-          <defs><clipPath id="map-clip-path"></clipPath></defs>
-        </svg>
-        <svg id="ridge-plots" />
-      </div>
+      <svg id="ridge-plots" />
     );
   }
 }
